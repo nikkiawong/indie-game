2b757c3f5dc440c352c2cf005d8ec27923681c97
Dan Mace and Nikki Wong <nikki@>
2018-09-18 11:58:08 -0700

    build site and do webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import './../node_modules/materialize-css/dist/css/materialize.min.css';
+// require('./../node_modules/materialize-css/dist/css/materialize.css');
+// window.jQuery = require('jquery');
+// window.$ = require('jquery');
+// require('./../node_modules/materialize-css/dist/js/materialize.js');
+// require('./../node_modules/materialize-css/js/init.js');
+
+import "materialize-loader";
+import './scss/input.scss';
+import './css/output.css';

13e7df52ed44a5337efecb54705b6bcec47f984c
Dan Mace and Nikki Wong <nikki@>
2018-09-19 15:17:51 -0700

    animate everything

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
         test: /\.scss$/,
         use: [
           'style-loader',
